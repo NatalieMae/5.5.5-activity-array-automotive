b9f78e8d42312f63d225256cdb743b89d7039e90
Natalie <nataliemae263@gmail.com>
2022-07-07 20:12:29 -0500

    entends operations

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,36 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+import  vehicle  from "./vehicle";
 
+const VehicleModule = require('./vehicle').vehicle
+
+class Car extends vehicle {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maximumPassengers = 6;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 200;
+        this.fuel = 100;
+        this.scheduleService = false
+    }
+
+    loadPassenger(num) {
+
+    }
+
+    startCar() {
+
+    }
+
+    scheduleService(mileage) {
+        if (this.mileage > 30000) {
+            this.scheduleService = true
+            return this.scheduleService;
+        }
+        return
+    }
+}
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+Car()
